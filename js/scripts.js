41666fe93de7db1f26d3578dd3038f3df7196abf
mitchnm <mitchndungu21@gmail.com>
2019-03-24 21:51:41 +0300

    added a function 2

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,7 @@ function triangletracker() {
     var firstNumber = parseInt(document.getElementById("firstNumber").value);
     var secondNumber = parseInt(document.getElementById("secondNumber").value);
     var thirdNumber = parseInt(document.getElementById("thirdNumber").value);
+     
+    var mitch = function(firstNumber,secondNumber,thirdNumber)  {
+        return (firstNumber + secondNumber > thirdNumber && firstNumber + thirdNumber > secondNumber && secondNumber + thirdNumber > firstNumber);
+        }
