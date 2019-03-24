5efefafdb1ae504bfb50f89edf3ff2ea4740f314
mitchnm <mitchndungu21@gmail.com>
2019-03-24 21:52:35 +0300

    added if

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,6 @@ function triangletracker() {
     var mitch = function(firstNumber,secondNumber,thirdNumber)  {
         return (firstNumber + secondNumber > thirdNumber && firstNumber + thirdNumber > secondNumber && secondNumber + thirdNumber > firstNumber);
         }
+        if (mitch(firstNumber,secondNumber,thirdNumber) && firstNumber <= 0 || secondNumber <= 0 || thirdNumber <= 0 || NaN )  {
+            alert ('THIS IS NOT A TRIANGLE!! TRY OTHER DIMENSIONS.');   
+            }
