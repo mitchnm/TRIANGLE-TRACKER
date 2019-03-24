d0d24e37c7407b38f7fee4ae5c90d9a883ba5e0d
mitchnm <mitchndungu21@gmail.com>
2019-03-24 21:50:45 +0300

    added a function

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+function triangletracker() {
+    var firstNumber = parseInt(document.getElementById("firstNumber").value);
+    var secondNumber = parseInt(document.getElementById("secondNumber").value);
+    var thirdNumber = parseInt(document.getElementById("thirdNumber").value);
